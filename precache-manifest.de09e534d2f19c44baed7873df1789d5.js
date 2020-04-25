a5705e832049b5c43bf6aaa987f3e81b407696d6
Shaked <shaked@7sjOF9KU9h.lan>
2020-04-25 23:33:56 +0300

    Updates

---
+++ precache-manifest.de09e534d2f19c44baed7873df1789d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755ff22338252c701ef45ebd96529559",
+    "url": "/%E2%80%8Bhttps://shakedkt.github.io/Mister-BITCoin-react-new/index.html"
+  },
+  {
+    "revision": "5b4a4ca69e8fa2ec99a1",
+    "url": "/%E2%80%8Bhttps://shakedkt.github.io/Mister-BITCoin-react-new/static/css/main.1bcac1ce.chunk.css"
+  },
+  {
+    "revision": "b3ee0ba812a645f68ba2",
+    "url": "/%E2%80%8Bhttps://shakedkt.github.io/Mister-BITCoin-react-new/static/js/2.f48c70cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/%E2%80%8Bhttps://shakedkt.github.io/Mister-BITCoin-react-new/static/js/2.f48c70cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4a4ca69e8fa2ec99a1",
+    "url": "/%E2%80%8Bhttps://shakedkt.github.io/Mister-BITCoin-react-new/static/js/main.b6654ccc.chunk.js"
+  },
+  {
+    "revision": "cfda41b0d82e1aa02f96",
+    "url": "/%E2%80%8Bhttps://shakedkt.github.io/Mister-BITCoin-react-new/static/js/runtime-main.0a3ab836.js"
+  },
+  {
+    "revision": "59489d4f7efbeeac12f27d41897837f7",
+    "url": "/%E2%80%8Bhttps://shakedkt.github.io/Mister-BITCoin-react-new/static/media/loading.59489d4f.svg"
+  }
+]);
